44047f2980764719c46adc999a71fc0751e0db46
Developer <112639963+xxxCam900xxx@users.noreply.github.com>
2025-05-15 14:46:48 +0200

    🐛 Fix: working frontend added

--- app/frontend/vite.config.js
+++ app/frontend/vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      include: "**/*.tsx",
-    }),
-    tailwindcss(),
-  ],
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 80,
+  },
 })
