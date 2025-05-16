7d9102f0c0f8ecf97d1b8ca10b63f9495d4e1b7b
Developer <112639963+xxxCam900xxx@users.noreply.github.com>
2025-05-16 11:24:09 +0200

    ✨ Database: added mockdata for testing

--- app/database/init.sql
+++ app/database/init.sql
@@ -37,3 +37,13 @@ CREATE TABLE Template (
     datum DATE NOT NULL,
     betrag NUMERIC(10, 2) NOT NULL
 );
+
+-- Tabellen Insert Budget
+INSERT INTO
+    Budget (titel, total_einnahmen, total_ausgaben)
+VALUES
+    ('Monat April', 3000.00, 2200.00),
+    ('Monat Mai', 3200.00, 2500.00),
+    ('Sommerurlaub', 1500.00, 1800.00),
+    ('Auto Reparatur', 0.00, 900.00),
+    ('Nebenprojekt', 500.00, 100.00);
