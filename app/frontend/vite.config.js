26f21b3bb6a691e7a6ef75497d7a8fb81839ac71
Developer <112639963+xxxCam900xxx@users.noreply.github.com>
2025-05-16 09:34:47 +0200

    ♻️ Frontend: cleanup + added functional tailwindcss

--- app/frontend/vite.config.js
+++ app/frontend/vite.config.js
@@ -1,11 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      include: "**/*.tsx",
+    }),
+    tailwindcss(),
+  ],
   server: {
-    host: '0.0.0.0',
-    port: 80,
+    host: true, // allows external access
+    hmr: {
+      clientPort: 443, // if you're using HTTPS tunnels
+    },
+    allowedHosts: ['nb-came.blue-elevator.ts.net'],
+    watch: {
+      usePolling: true,
+    },
   },
 })
