63f088aa13b4db921e83e4ab9e379f1db8cfeb98
Developer <112639963+xxxCam900xxx@users.noreply.github.com>
2025-05-16 10:42:04 +0200

    ✨ Frontend: visual interface build without any functions yet

--- app/frontend/src/App.jsx
+++ app/frontend/src/App.jsx
@@ -1,8 +1,22 @@
+import BugetSelectionView from "./assets/components/BugetSelectionView"
+import DashboardView from "./assets/components/DashboardView"
+
 function App() {
   return (
-    <>
-      
-    </>
+    <main className="flex flex-row h-full w-full">
+      {/* Dashboard Section */}
+      <section
+        className="w-2/3 bg-sky-300 flex justify-center items-center p-5"
+      >
+        <DashboardView />
+      </section>
+      {/* Folder Selection Section */}
+      <section
+        className="w-1/3 flex flex-col h-full"
+      >
+        <BugetSelectionView />
+      </section>
+    </main>
   )
 }
 
---
+++ app/frontend/src/assets/components/DashboardView.jsx
@@ -0,0 +1,11 @@
+function DashboardView() {
+    return (
+        <>
+            <h1 className="text-4xl text-white">
+                Overall Dashboard Coming Soon...
+            </h1>
+        </>
+    )
+}
+
+export default DashboardView
