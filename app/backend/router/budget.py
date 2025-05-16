5021c1449b2c602cec62c8337757b46cf074d2c7
Developer <112639963+xxxCam900xxx@users.noreply.github.com>
2025-05-16 13:53:15 +0200

    ✨ Backend: added endpoint to create new empty budgets

--- app/backend/router/budget.py
+++ app/backend/router/budget.py
@@ -1,16 +1,24 @@
 from fastapi import APIRouter, Depends
-from service.budgetService import getAllBudgets, deleteBudgetById
+from service.budgetService import getAllBudgets, deleteBudgetById, createBudget
 from database.database import get_db
 from sqlalchemy.ext.asyncio import AsyncSession
+from pydantic import BaseModel
 
 router = APIRouter(
     prefix="/budget",
     tags=["budget"]
 )
 
+class BudgetModel(BaseModel):
+    titel: str
+
 @router.get("/")
 async def get_All(db: AsyncSession = Depends(get_db)):
     return await getAllBudgets(db)
+
+@router.post("/")
+async def add_budget(body: BudgetModel, db: AsyncSession = Depends(get_db)):
+    return await createBudget(body, db)
 
 @router.delete("/{budget_id}")
 async def delete_BudgetById(budget_id, db: AsyncSession = Depends(get_db)):
--- app/backend/service/budgetService.py
+++ app/backend/service/budgetService.py
@@ -1,11 +1,17 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
-from sqlalchemy import delete
+from sqlalchemy import delete, insert
 from database.models import Budget
 
 async def getAllBudgets(db: AsyncSession):
     result = await db.execute(select(Budget).order_by(Budget.budget_id))
     return result.scalars().all()
+
+async def createBudget(body, db: AsyncSession):
+    insertStmt = insert(Budget).values(titel=body.titel)
+    await db.execute(insertStmt)
+    await db.commit()
+    return
 
 async def deleteBudgetById(id: int, db: AsyncSession):
     deleteStmt = delete(Budget).where(Budget.budget_id == int(id))
