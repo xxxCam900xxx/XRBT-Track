4132a747b5bcb8c044d6629b63eef5afc203a49c
Developer <112639963+xxxCam900xxx@users.noreply.github.com>
2025-05-16 13:10:21 +0200

    💄 Frontend: budgetView styling imporved

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -3,16 +3,16 @@ import DashboardView from "./assets/components/DashboardView"
 
 function App() {
   return (
-    <main className="flex flex-row h-full w-full">
+    <main className="flex flex-row h-full w-full bg-sky-300">
       {/* Dashboard Section */}
       <section
-        className="w-2/3 bg-sky-300 flex justify-center items-center p-5"
+        className="w-2/3 flex justify-center items-center p-5"
       >
         <DashboardView />
       </section>
       {/* Folder Selection Section */}
       <section
-        className="w-1/3 flex flex-col h-full"
+        className="w-1/3 flex flex-col h-full rounded-l-2xl overflow-hidden bg-white"
       >
         <BugetSelectionView />
       </section>
--- app/frontend/src/assets/components/BugetSelectionView.tsx
+++ app/frontend/src/assets/components/BugetSelectionView.tsx
@@ -28,25 +28,25 @@ function BudgetSelectionView() {
   return (
     <>
       {/* Budget Folder List */}
-      <section className="bg-green-300 w-full h-full max-h-full overflow-y-auto flex flex-col p-5 gap-4">
+      <section className="w-full h-full max-h-full overflow-y-auto flex flex-col p-5 gap-5">
         {data.map((budget) => (
           <div
             key={budget.budget_id}
-            className="bg-violet-300 flex flex-row items-center justify-between p-5 rounded-xl relative gap-5 cursor-pointer"
+            className="border-gray-200 border flex flex-row items-center justify-between p-5 rounded-xl relative gap-5 cursor-pointer shadow-lg"
           >
             <div className="flex flex-col w-full gap-2">
               <h1 className="text-2xl font-semibold">{budget.titel}</h1>
-              <div className="flex flex-col justify-center w-full items-center rounded-xl overflow-hidden">
+              <div className="flex flex-col justify-center w-full items-center rounded-lg overflow-hidden">
                 <div className="flex flex-row justify-around w-full">
-                  <p className="border-b w-full text-center p-1 bg-green-300">Einnahmen: {budget.total_einnahmen}</p>
-                  <p className="border-b w-full text-center p-1 bg-red-300">Ausgaben: {budget.total_ausgaben}</p>
+                  <p className="border-b border-gray-300 w-full text-center p-1 bg-green-300">Einnahmen: {budget.total_einnahmen}</p>
+                  <p className="border-b border-gray-300 w-full text-center p-1 bg-red-300">Ausgaben: {budget.total_ausgaben}</p>
                 </div>
-                <h1 className="w-full text-center p-1 bg-green-300">Umsatz: {budget.total_umsatz}</h1>
+                <h1 className="w-full text-center p-1 bg-gray-100">Umsatz: {budget.total_umsatz}</h1>
               </div>
             </div>
             <div className="flex h-full items-start">
-              <button className="bg-red-400 text-md aspect-square w-[40px] rounded-xl cursor-pointer">
-                <i className="fa-solid fa-trash"></i>
+              <button className="bg-sky-400 text-md aspect-square w-[40px] rounded-xl cursor-pointer">
+                <i className="fa-solid fa-trash text-white"></i>
               </button>
             </div>
           </div>
@@ -54,9 +54,9 @@ function BudgetSelectionView() {
       </section>
 
       {/* Tools */}
-      <section className="bg-red-300 w-full h-[100px] p-5 flex flex-row gap-5 items-center justify-center">
-        <button className="px-5 h-fit w-3/5 py-2 bg-yellow-300 rounded-md cursor-pointer">Create</button>
-        <button className="px-5 py-2 h-fit bg-yellow-300 rounded-md cursor-pointer">Credits</button>
+      <section className="w-full h-[100px] p-5 flex flex-row gap-5 items-center justify-center">
+        <button className="shadow-xl px-5 h-fit w-3/5 py-2 bg-sky-300 rounded-md cursor-pointer text-white font-semibold">Create</button>
+        <button className="shadow-xl px-5 py-2 h-fit bg-sky-300 rounded-md cursor-pointer text-white font-semibold">Credits</button>
       </section>
     </>
   );
