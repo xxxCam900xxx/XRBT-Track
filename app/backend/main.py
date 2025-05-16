3d0a7ddab58afacd2a03d9386084ad4a7133d101
Developer <112639963+xxxCam900xxx@users.noreply.github.com>
2025-05-16 08:44:19 +0200

    ✨ Backend: added root redirect to documentation swagger

--- app/backend/main.py
+++ app/backend/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 
 app = FastAPI()
 
+# Makes a direct Redirect to Documentation when opening lochalhost:8000
 @app.get("/")
 async def read_root():
-    return {"Hello": "World"}
+    return RedirectResponse("/docs")
