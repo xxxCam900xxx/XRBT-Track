c2ccb8a3ce202cf0cb4c7cfdb2fdcce01f299940
Developer <112639963+xxxCam900xxx@users.noreply.github.com>
2025-06-10 16:47:47 +0200

    ✨ Backend: added update overall month endpoint

--- app/backend/router/month.py
+++ app/backend/router/month.py
@@ -1,12 +1,17 @@
 from fastapi import APIRouter, Depends
 from database.database import get_db
 from sqlalchemy.ext.asyncio import AsyncSession
-from service.monthService import getAllMonthsByBudgetID, getAllMonths, getAllMonthsByID
+from service.monthService import getAllMonthsByBudgetID, getAllMonths, getAllMonthsByID, updateMonthsByID
+from pydantic import BaseModel
 
 router = APIRouter(
     prefix="/month",
     tags=["month"]
 )
+
+class MonthInfos(BaseModel):
+    total_ausgaben: float
+    total_einnahmen: float
 
 @router.get("/")
 async def get_All_Months(db: AsyncSession = Depends(get_db)):
@@ -19,3 +24,7 @@ async def get_All_Months_By_BudgetID(budget_id, db: AsyncSession = Depends(get_d
 @router.get("/info/{month_id}")
 async def get_Month_Details_By_ID(month_id, db: AsyncSession = Depends(get_db)):
     return await getAllMonthsByID(month_id, db)
+
+@router.patch("/info/{month_id}")
+async def get_Month_Details_By_ID(month_id, body: MonthInfos, db: AsyncSession = Depends(get_db)):
+    return await updateMonthsByID(month_id, body, db)
--- app/backend/service/monthService.py
+++ app/backend/service/monthService.py
@@ -1,5 +1,6 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
+from sqlalchemy import update
 from database.models import Monat
 
 async def getAllMonths(db: AsyncSession):
@@ -13,4 +14,13 @@ async def getAllMonthsByBudgetID(id, db: AsyncSession):
 async def getAllMonthsByID(id, db: AsyncSession):
     result = await db.execute(select(Monat).where(Monat.monat_id == int(id)))
     return result.scalars().all()
+
+async def updateMonthsByID(id, body, db: AsyncSession):
+    update_stmt =  update(Monat).where(Monat.monat_id == int(id)).values(
+        total_ausgaben=int(body.total_ausgaben),
+        total_einnahmen=int(body.total_einnahmen)
+    )
+    await db.execute(update_stmt)
+    await db.commit()
+    return
     
