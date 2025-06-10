bdabc87bfc043166f1fc4874aabb2fe2fc2b9d64
Developer <112639963+xxxCam900xxx@users.noreply.github.com>
2025-06-10 13:25:30 +0200

    ✨ Backend: added Endpoints for creating new booking

--- app/backend/router/booking.py
+++ app/backend/router/booking.py
@@ -1,12 +1,20 @@
 from fastapi import APIRouter, Depends
 from database.database import get_db
 from sqlalchemy.ext.asyncio import AsyncSession
-from service.bookingService import getAllBookings, getAllBookingsByTypeAndId
+from service.bookingService import getAllBookings, getAllBookingsByTypeAndId, addNewBooking
+from pydantic import BaseModel
 
 router = APIRouter(
     prefix="/booking",
     tags=["booking"]
 )
+
+class BookingModel(BaseModel):
+    titel: str
+    datum: str
+    typ: str
+    betrag: float
+    monat_id: str
 
 @router.get("/")
 async def get_All_Bookings(db: AsyncSession = Depends(get_db)):
@@ -15,3 +23,7 @@ async def get_All_Bookings(db: AsyncSession = Depends(get_db)):
 @router.get("/{id}/{typ}")
 async def get_All_Bookings_By_Type_And_Id(id, typ, db: AsyncSession = Depends(get_db)):
     return await getAllBookingsByTypeAndId(id, typ, db)
+
+@router.post("/")
+async def add_New_Booking(body: BookingModel, db: AsyncSession = Depends(get_db)):
+    return await addNewBooking(body, db)
--- app/backend/service/bookingService.py
+++ app/backend/service/bookingService.py
@@ -1,5 +1,7 @@
+from datetime import date
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
+from sqlalchemy import insert
 from database.models import Buchung
 
 async def getAllBookings(db: AsyncSession):
@@ -9,3 +11,18 @@ async def getAllBookings(db: AsyncSession):
 async def getAllBookingsByTypeAndId(id, typ, db: AsyncSession):
     result = await db.execute(select(Buchung).where(Buchung.typ == typ).where(Buchung.monat_id == int(id)))
     return result.scalars().all()
+
+async def addNewBooking(body, db: AsyncSession):
+    # Booking erstellen
+    insert_stmt = insert(Buchung).values(
+        monat_id=int(body.monat_id), 
+        typ=body.typ,
+        titel=body.titel, 
+        datum=date.fromisoformat(body.datum), 
+        betrag=float(body.betrag)
+    )
+    await db.execute(insert_stmt)
+    await db.commit()
+    return
+    
+    
