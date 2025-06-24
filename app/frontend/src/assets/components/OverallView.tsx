b7c5a91320fcbd2cee917c2843b4389475ebec86
Developer <112639963+xxxCam900xxx@users.noreply.github.com>
2025-06-24 10:41:33 +0200

    ✨ Frontend: OverallView updated to newest Design

--- app/frontend/src/assets/components/OverallView.tsx
+++ app/frontend/src/assets/components/OverallView.tsx
@@ -80,74 +80,45 @@ function OverallView() {
   if (error) return <p>Error: {error}</p>;
 
   return (
-    <main className="flex flex-row h-full w-full bg-sky-300">
-      {/* Dashboard Section */}
-      <section
-        className="w-2/3 flex flex-col justify-center items-center p-5 gap-5"
-      >
-        <h1 className='text-white text-4xl'>Statistic Coming Soon...</h1>
-        <div className='flex gap-5'>
-          <div className='shadow-xl flex flex-col items-center bg-green-100 rounded-md py-2 px-5'>
-            <h2>Totale Einnahen</h2>
-            <p>{totalIncomes}</p>
-          </div>
-          <div className='shadow-xl flex flex-col items-center bg-red-100 rounded-md py-2 px-5'>
-            <h2>Totale Ausgaben</h2>
-            <p>{totalOutgoings}</p>
-          </div>
-          <div className='shadow-xl flex flex-col items-center bg-white rounded-md py-2 px-5'>
-            <h2>Totaler Umsatz</h2>
-            <p>{total}</p>
-          </div>
-        </div>
-      </section>
-      {/* Folder Selection Section */}
-      <section
-        className="w-1/3 flex flex-col h-full rounded-l-2xl overflow-hidden bg-white"
-      >
+    <main className="flex flex-row h-full w-full primary-background-color">
+
+      {/* Navigation Bar Section */}
+      <section className="w-1/4 flex flex-col h-full secondary-background-color shadow-2xl">
+
+        {/* Banner Image */}
+        <img src="/images/XRBT-Banner.png" alt="XRBT-Banner" className='w-full' />
+
         {/* Budget Folder List */}
-        <section className="w-full h-full max-h-full overflow-y-auto flex flex-col p-5 gap-5">
+        <section className="w-full h-full max-h-full overflow-y-auto flex flex-col p-5 gap-3">
           {data.map((budget) => (
             <div
               key={budget.budget_id}
-              className="border-gray-200 border flex flex-row items-center justify-between p-5 rounded-xl relative gap-5 cursor-pointer shadow-lg"
+              className="flex flex-row items-center justify-between p-3 rounded-xl relative gap-5 cursor-pointer primary-background-color"
               onClick={() => handleClickBudgetPlan(budget.budget_id, budget.titel)}
             >
-              <div className="flex flex-col w-full gap-2">
-                <h1 className="text-2xl font-semibold">{budget.titel}</h1>
-                <div className="flex flex-col justify-center w-full items-center rounded-lg overflow-hidden">
-                  <div className="flex flex-row justify-around w-full">
-                    <p className="border-b border-gray-300 w-full text-center p-1 bg-green-300">{budget.total_einnahmen}</p>
-                    <p className="border-b border-gray-300 w-full text-center p-1 bg-red-300">{budget.total_ausgaben}</p>
-                    <p className="border-b border-gray-300 w-full text-center p-1 bg-gray-100">= {budget.total_umsatz}</p>
-                  </div>
-                </div>
-              </div>
-              <div className="flex h-full items-start">
-                <button
-                  className="bg-sky-400 text-md aspect-square w-[40px] rounded-xl cursor-pointer"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    e.stopPropagation();
-                    deleteBudgetById(budget.budget_id);
-                  }}
-                >
-                  <i className="fa-solid fa-trash text-white"></i>
-                </button>
-              </div>
+              <h1 className="text-2xl text-white font-semibold">{budget.titel}</h1>
+              <button
+                className="bg-white text-md aspect-square w-[40px] rounded-xl cursor-pointer"
+                onClick={(e) => {
+                  e.preventDefault();
+                  e.stopPropagation();
+                  deleteBudgetById(budget.budget_id);
+                }}
+              >
+                <i className="fa-solid fa-trash text-lg primary-background-textcolor"></i>
+              </button>
             </div>
           ))}
         </section>
 
-        {/* Tools */}
+        {/* Create Tool */}
         <section className="w-full h-[100px] p-5 flex flex-row gap-5 items-center justify-center">
           <button
-            className="shadow-xl px-5 h-fit w-3/5 py-2 bg-sky-300 rounded-md cursor-pointer text-white font-semibold"
+            className="px-5 h-fit w-full py-2 rounded-md cursor-pointer text-white text-xl font-semibold primary-background-color"
             onClick={() => setDisplayNewBudgetPopUp(true)}
           >
-            Create
+            Neues Budget erstellen
           </button>
-          <a href='/credits' className="shadow-xl px-5 py-2 h-fit bg-sky-300 rounded-md cursor-pointer text-white font-semibold">Credits</a>
         </section>
 
         <CreateNewBudgetPopUp
@@ -155,6 +126,51 @@ function OverallView() {
           setDisplayNewBudgetPopUp={setDisplayNewBudgetPopUp}
         />
       </section>
+
+
+      {/* Dashboard Section */}
+      <aside className='flex flex-col gap-5 w-3/4 p-10'>
+        {/* Totals */}
+        <section className='flex gap-5'>
+          <div className='flex flex-col gap-2 p-3 secondary-background-color w-full h-fit rounded-md'>
+            <div className='flex flex-row gap-2 w-full items-center'>
+              <i className="fa-solid fa-piggy-bank text-3xl primary-background-textcolor"></i>
+              <h1 className='text-3xl font-semibold primary-background-textcolor'>Einnahmen</h1>
+            </div>
+            <div className='bg-emerald-50 text-emerald-600 text-center text-3xl font-semibold p-3 rounded-md'>
+              +{totalIncomes} CHF
+            </div>
+          </div>
+          <div className='flex flex-col gap-2 p-3 secondary-background-color w-full h-fit rounded-md'>
+            <div className='flex flex-row gap-2 w-full items-center'>
+              <i className="fa-solid fa-cart-shopping text-3xl primary-background-textcolor"></i>
+              <h1 className='text-3xl font-semibold primary-background-textcolor'>Ausgaben</h1>
+            </div>
+            <div className='bg-red-50 text-red-600 text-center text-3xl font-semibold p-3 rounded-md'>
+              {totalOutgoings} CHF
+            </div>
+          </div>
+          <div className='flex flex-col gap-2 p-3 secondary-background-color w-full h-fit rounded-md'>
+            <div className='flex flex-row gap-2 w-full items-center'>
+              <i className="fa-solid fa-chart-simple text-3xl primary-background-textcolor"></i>
+              <h1 className='text-3xl font-semibold primary-background-textcolor'>Einnahmen</h1>
+            </div>
+            <div
+              className={`${total < 0 ? 'bg-red-50 text-red-600' : 'bg-emerald-50 text-emerald-600'
+                } text-center text-3xl font-semibold p-3 rounded-md`}
+            >
+              {total} CHF
+            </div>
+          </div>
+        </section>
+        <section className='flex items-center w-full h-full text-white text-5xl justify-center'>
+          Statistics still in Progress...
+        </section>
+
+
+      </aside>
+
+
     </main>
   );
 }
