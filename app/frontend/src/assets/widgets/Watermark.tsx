3f19b7b370dafe315b7863297afb538f21e57b18
Developer <112639963+xxxCam900xxx@users.noreply.github.com>
2025-06-24 10:07:56 +0200

    ✨ Frontend: added Firststep of new Design. added Watermark

---
+++ app/frontend/src/assets/widgets/Watermark.tsx
@@ -0,0 +1,11 @@
+function Watermark() {
+
+    return (
+        <a href="/credits" className="fixed bottom-5 right-5 w-[50px] h-[50px] rounded overflow-hidden">
+            <img src="/images/XRBT-Logo.png" alt="XRBT-Watermark" className="w-full h-full" />
+        </a>
+    )
+
+}
+
+export default Watermark;
--- app/frontend/src/main.jsx
+++ app/frontend/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.js'
+import Watermark from './assets/widgets/Watermark.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Watermark />
   </StrictMode>,
 )
