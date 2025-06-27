6908810581d10f9fa5d1e8c5f820291ebd0a19d6
Developer <112639963+xxxCam900xxx@users.noreply.github.com>
2025-06-27 14:08:42 +0200

    ✨ Frontend: added MobileView for DashboardTotals

--- app/frontend/src/assets/components/ui/dashboardTotals.tsx
+++ app/frontend/src/assets/components/ui/dashboardTotals.tsx
@@ -43,7 +43,7 @@ const DashboardTotals: React.FC<DashboardTotalsProps> = ({
                 <div className="flex flex-col gap-2 p-3 secondary-background-color w-full h-fit rounded-md">
                     <div className="flex flex-row gap-2 w-full items-center">
                         <i className="fa-solid fa-chart-simple text-3xl primary-background-textcolor"></i>
-                        <h1 className="text-3xl font-semibold primary-background-textcolor">Saldo</h1>
+                        <h1 className="text-3xl font-semibold primary-background-textcolor">Umsatz</h1>
                     </div>
                     <div
                         className={`${total < 0 ? "bg-red-50 text-red-600" : "bg-emerald-50 text-emerald-600"
@@ -57,30 +57,32 @@ const DashboardTotals: React.FC<DashboardTotalsProps> = ({
             {/* Mobile Ansicht */}
             <section className="flex gap-2 w-full lg:hidden">
 
-                <div className="flex flex-row gap-2 p-3 secondary-background-color w-full h-fit rounded-md">
-                    <div className="flex flex-row gap-2 items-center">
-                        {selected === "einnahmen" && (
-                            <i className="fa-solid fa-piggy-bank text-4xl primary-background-textcolor"></i>
-                        )}
-                        {selected === "ausgaben" && (
-                            <i className="fa-solid fa-cart-shopping text-4xl primary-background-textcolor"></i>
-                        )}
-                        {selected === "total" && (
-                            <i className="fa-solid fa-chart-simple text-4xl primary-background-textcolor"></i>
-                        )}
-                    </div>
+                <div className="flex flex-col md:flex-row gap-2 p-3 secondary-background-color w-full h-fit rounded-md">
+                    <div className="flex gap-2 w-full">
+                        <div className="flex flex-row gap-2 items-center">
+                            {selected === "einnahmen" && (
+                                <i className="fa-solid fa-piggy-bank text-4xl primary-background-textcolor"></i>
+                            )}
+                            {selected === "ausgaben" && (
+                                <i className="fa-solid fa-cart-shopping text-4xl primary-background-textcolor"></i>
+                            )}
+                            {selected === "total" && (
+                                <i className="fa-solid fa-chart-simple text-4xl primary-background-textcolor"></i>
+                            )}
+                        </div>
 
-                    <div
-                        className={`${selected === "ausgaben"
+                        <div
+                            className={`${selected === "ausgaben"
                                 ? "bg-red-50 text-red-600"
                                 : selected === "total" && total < 0
                                     ? "bg-red-50 text-red-600"
                                     : "bg-emerald-50 text-emerald-600"
-                            } text-center text-3xl font-semibold w-full p-3 rounded-md`}
-                    >
-                        {selected === "einnahmen" && `+${totalIncomes} CHF`}
-                        {selected === "ausgaben" && `${totalOutgoings} CHF`}
-                        {selected === "total" && `${total} CHF`}
+                                } text-center text-3xl font-semibold w-full p-3 rounded-md`}
+                        >
+                            {selected === "einnahmen" && `+${totalIncomes} CHF`}
+                            {selected === "ausgaben" && `${totalOutgoings} CHF`}
+                            {selected === "total" && `${total} CHF`}
+                        </div>
                     </div>
                     <select
                         className="p-2 rounded-md bg-white text-lg"
